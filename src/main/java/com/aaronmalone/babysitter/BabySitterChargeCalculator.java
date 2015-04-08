5a34a5011dd5bcf0e05b7cf9e04f1bc5b15230a5
Aaron Malone <aaronmalone@gmail.com>
2015-04-08 08:50:39 -0400

    Added more clarifying comments on the charge calculation

--- src/main/java/com/aaronmalone/babysitter/BabySitterChargeCalculator.java
+++ src/main/java/com/aaronmalone/babysitter/BabySitterChargeCalculator.java
@@ -20,17 +20,21 @@ import java.time.LocalTime;
  * kata:
  * <ul>
  * <li>Bedtime never occurs after midnight, but may happen any time between the
- * start time and midnight (inclusive). Every night have a bedtime.</li>
+ * start time and midnight (inclusive). Every night has a bedtime.</li>
  * </ul>
  * <p/>
  * The babysitter gets paid for full hours, and there are no fractional hours
  * in the calculation of the nightly charge. This does not specify how
- * fractions of an hour should be figured. I will assume the following are true:
+ * fractions of an hour should be figured. This is how I will interpret it:
  * <ul>
- * <li>Each rate will be multiplied by a whole number of hours to calculate
- * the overall charge.</li>
- * <li>The number of hours for each rate, added together, should equal the
- * total number of hours for the night (rounded to the nearest whole).</li>
+ * <li>For each hour, if the babysitter works any part of that hour,
+ * then the babysitter is paid for the whole hour (e.g. if the babysitter starts
+ * at 7:55pm and finishes at 8:05pm, the babysitter is paid for the full
+ * 7-o'clock hour and the full 8-o'clock hour).</li>
+ * <li>If the babysitter works at the pre-bedtime rate for any part of an hour,
+ * then the babysitter charges the pre-bedtime rate for the entire hour (e.g.
+ * if bedtime is at 10:05pm, then the babysitter charges the pre-bedtime rate
+ * for the entire 10-o'clock hour).</li>
  * </ul>
  */
 @SuppressWarnings("OctalInteger") //so we can use "00" for minutes
@@ -40,6 +44,8 @@ public class BabySitterChargeCalculator {
 
 	/**
 	 * Calculate the charge for a night of babysitting.
+	 * The specific rules for calculating the charge are in the Javadoc for
+	 * {@link BabySitterChargeCalculator}.
 	 *
 	 * @param startTime the time at which babysitting begins.
 	 * @param bedTime   the time when the children go to bed.
